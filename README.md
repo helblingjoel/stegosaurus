82698082f8fd25bc1debfc2f056131acfe354cd9
Joel Helbling <helblingjoel@gmail.com>
2022-01-27 23:02:20 +0000

    Wrote messages into each full square

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # stegosaurus
 
-## any-steganography
-
-[npm page](https://www.npmjs.com/package/any-steganography)
+## [any-steganography](https://www.npmjs.com/package/any-steganography)
 
 Last updated: 26/01/2022
-
+Weekly Downloads: 130
 Dependencies: 0
 
 ### Usability
@@ -13,3 +11,39 @@ Dependencies: 0
 -   Really straight forward
 -   Not very customisable
 -   Requires image to be cropped down first
+
+## ~~[node-images](https://www.npmjs.com/package/images)~~
+
+<b>Poor documentation</b>
+
+Last updated: 1 year ago
+
+Weekly downloads: 9000
+
+Dependencies: 0
+
+## ~~[image-clipper](https://www.npmjs.com/package/image-clipper)~~
+
+<b>Can't crop and merge</b>
+
+Last updated: 3 years ago
+
+Weekly downloads: 2000
+
+Dependencies: 0
+
+## ~~[sharp](https://github.com/lovell/sharp)~~
+
+<b>Can't crop and merge</b>
+
+Last updated: 1 year ago
+
+Weekly downloads: 9000
+
+Dependencies: 0
+
+Can crop images just fine
+
+## Join Images
+
+Can merge together quite easily
--- index.js
+++ index.js
@@ -5,33 +5,82 @@ const fs = require("fs");
 
 const key = "abcdefghabcdefghabcdefghabcdefgh";
 
-embedMessage = (filepath, message) => {
+embedMessage = (filepath, message, outpath) => {
 	const file = steg.write(filepath, message, key);
-	fs.writeFileSync("./sample_images/test-processed.jpg", file);
+	fs.writeFileSync(outpath, file);
 };
 
 const decryptImage = (filepath) => {
-	const message = steg.decode(filepath, "jpg", key);
+	const message = steg.decode(filepath, "png", key);
 	return message;
 };
 
-const cropImage = (filepath, width, height, widthStart = 0, heightStart = 0) => {
-	sharp(filepath)
+const cropImage = (filepath, outpath, width, height, widthStart = 0, heightStart = 0) => {
+	return sharp(filepath)
 		.extract({ width: width, height: height, left: widthStart, top: heightStart })
-		.toFile("./sample_images/cropped.jpg")
+		.toFile(outpath)
 		.catch((err) => console.log(err));
 };
 
-const mergeImage = (image1, image2, stitchLocation) => {
-	if (stitchLocation !== "horizontal" || stitchLocation !== "vertical")
+const mergeImage = (image1, image2, stitchLocation, outpath) => {
+	if (stitchLocation !== "horizontal" && stitchLocation !== "vertical")
 		throw Error("Stitch location is neither horizontal nor vertical");
-	joinImage([image1, image2], { direction: "horizontal" }).then((img) => {
-		img.toFile("out.png");
+
+	joinImage([image1, image2], { direction: stitchLocation }).then((img) => {
+		img.toFile(outpath);
 	});
 };
 
-cropImage("./sample_images/test.jpg", 50, 50);
+const main = async () => {
+	const file = "./sample_images/test.jpg";
+	const metadata = await sharp(file).metadata();
 
-mergeImage("./sample_images/test.jpg", "./sample_images/test.jpg", "somewhere");
+	let width = metadata.width;
+	let height = metadata.height;
 
-embedMessage("./sample_images/test.jpg", "hello there");
+	let xProcessed = 0;
+	let yProcessed = 0;
+	for (let x = 0; x <= width - 50; x += 50) {
+		yProcessed = 0;
+		for (let y = 0; y <= height - 50; y += 50) {
+			await cropImage(
+				file,
+				`./sample_images/cropped-${xProcessed}-${yProcessed}.png`,
+				50,
+				50,
+				x,
+				y
+			);
+			embedMessage(
+				`./sample_images/cropped-${xProcessed}-${yProcessed}.png`,
+				`message ${xProcessed}-${yProcessed}`,
+				`./sample_images/cropped-${xProcessed}-${yProcessed}-done.png`
+			);
+			fs.unlinkSync(`./sample_images/cropped-${xProcessed}-${yProcessed}.png`);
+			yProcessed++;
+		}
+		xProcessed++;
+	}
+
+	xProcessed = 0;
+	yProcessed = 0;
+
+	for (let x = 0; x <= width - 50; x += 50) {
+		yProcessed = 0;
+		for (let y = 0; y <= height - 50; y += 50) {
+			console.log(
+				decryptImage(
+					`./sample_images/cropped-${xProcessed}-${yProcessed}-done.png`
+				)
+			);
+			yProcessed++;
+		}
+		xProcessed++;
+	}
+};
+
+main();
+
+// mergeImage("./sample_images/test.jpg", "./sample_images/test.jpg", "vertical");
+
+// embedMessage("./sample_images/test.jpg", "hello there");
