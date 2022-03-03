1b93861c06249a92b1af8dacc8e4b6d423a4b5bf
Joel Helbling <helblingjoel@gmail.com>
2022-03-03 15:39:06 +0000

    Can read regular QR Codes

--- read.js
+++ read.js
@@ -1,2 +1,22 @@
-import jsQR from "jsqr";
-import fs from "fs";
+const jsQR = require("jsqr");
+const getPixels = require("get-pixels");
+const size = require("image-size");
+
+
+function readQR(data, width, height){
+    const code = jsQR(data, width, height);
+    console.log(code);
+    if (code)
+        console.log(code.binaryData, code.data);
+}
+
+getPixels(filepath = "in-bordered.png", (err, pixels) => {
+        if (err){
+            console.log(`An error occured:\n${err}`);
+            return;
+        }
+
+        const dimension = size.imageSize(filepath)
+        readQR(pixels.data, dimension.width, dimension.height);
+    }
+);
