390a39e26d038ff7b819830b8f855c233e2923be
unknown <helblingjoel@gmail.com>
2022-03-14 13:57:53 +0000

    Set up for demo

--- read.js
+++ read.js
@@ -1,10 +1,12 @@
 const barcodeXpress = require("barcode-js");
 
-async function read() {
-	const results = await barcodeXpress.analyze("in-bordered.png", {
+async function read(filepath) {
+	const results = await barcodeXpress.analyze(filepath, {
 		type: barcodeXpress.BarcodeType.MICROQRCODE,
 	});
-	console.log(results[0]);
+
+	if (!results[0]) return false;
+
 	return {
 		raw: results[0].valueRaw,
 		text: results[0].value.split(" UNLICENSED accusoft.com")[0],
@@ -12,8 +14,14 @@ async function read() {
 }
 
 async function main() {
-	const values = await read();
-	console.log(values);
+	const out = await read("./files/out.png");
+	console.log("out.png / in-transparent.png\n", out);
+
+	const whiteBg = await read("./files/in-white-bg.png");
+	console.log("in-white-bg.png\n", whiteBg);
+
+	const bordered = await read("./files/in-bordered.png");
+	console.log("in-bordered.png\n", bordered);
 }
 
 main();
