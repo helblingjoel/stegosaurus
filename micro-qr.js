8587d94cfde89b7ad624499dea57bf1b8722bcdc
Spatchy <spatchy@spatchy.net>
2022-03-21 19:20:46 +0000

    made it generate 100 at a time for testing

--- micro-qr.js
+++ micro-qr.js
@@ -1,27 +1,44 @@
 const bwipjs = require('bwip-js');
 const fs = require("fs")
+const { v4: uuidv4 } = require('uuid')
 
 // Converts hex to bwip's ^NNN syntax - NNN is a 3 digit decimal number representation of a byte (i.e. an ascii char code)
 function hexToNNN(data) {
   return data.match(/.{1,2}/g).map(i => "^" + parseInt(i, 16).toString(10).padStart(3, "0")).join("")
+  
 }
-const uuidMinus = "a5d3d1513e74466881678dc5553c2a" //UUID minus last byte
-console.log(hexToNNN(uuidMinus))
 
-bwipjs.toBuffer({
-        bcid: "microqrcode",
-        text: hexToNNN(uuidMinus),
-        scale: 1,
-        eclevel: "L",
-        parse: true // Tell bwip to parse NNN escape codes
+let successes = 0
+let failures = 0
+
+function doEncode(uuidMinus) {
+  bwipjs.toBuffer({
+    bcid: "microqrcode",
+    text: hexToNNN(uuidMinus),
+    scale: 1,
+    eclevel: "L",
+    parse: true // Tell bwip to parse NNN escape codes
     }, function (err, png) {
-        if (err) {
-            throw err
-        } else {
-          fs.writeFileSync("out.png", png)
-            // `png` is a Buffer
-            // png.length           : PNG file length
-            // png.readUInt32BE(16) : PNG image width
-            // png.readUInt32BE(20) : PNG image height
-        }
+      console.log(uuidMinus)
+      console.log(hexToNNN(uuidMinus))
+      if (err) {
+        failures++
+        //throw err
+      } else {
+        fs.writeFileSync("out/" + uuidMinus + ".png", png)
+        console.log("success")
+        successes++
+      }
     });
+}
+
+//doEncode("4b3900000000000000000000000000")
+
+const interval = setInterval(function() {
+  const uuidMinus = uuidv4().replace(/-/g, "").slice(0, -2)
+  doEncode(uuidMinus) 
+  if(successes === 100) {
+    clearInterval(interval)
+    console.log("successes: " + successes + " | failures: " + failures)
+  }
+}, 10)
